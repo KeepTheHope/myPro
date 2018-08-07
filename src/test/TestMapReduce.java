151db0b7f8f28b2ba5ee92f4a67087a210c39b7c
ghy <1944462641@qq.com>
2018-08-07 10:49:09 +0800

    MapReduce分析数据

---
+++ src/test/TestMapReduce.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+public class TestMapReduce {
+	public static void main(String[] args) {
+		List<Order> list = new ArrayList<Order>();
+		list.add(new Order("手机", 2999.9, 100));
+		list.add(new Order("电脑", 4999.9, 200));
+		list.add(new Order("奥迪", 99999.9, 50));
+		list.add(new Order("鼠标", 99.9, 200));
+		list.add(new Order("键盘", 999.9, 200));
+		DoubleSummaryStatistics summ = list.stream().mapToDouble((obj) -> obj.getPrice() * obj.getAmount())
+				.summaryStatistics();
+		System.out.println("总量:" + summ.getCount());
+		System.out.println("平均值:" + summ.getAverage());
+		System.out.println("最大:" + summ.getMax());
+		System.out.println("最小:" + summ.getMin());
+		System.err.println(99999.9*50);
+
+	}
+
+}
+
+class Order {
+	private String title;
+	private Double price;
+	private Integer amount;
+
+	public Order(String title, Double price, Integer amount) {
+		super();
+		this.title = title;
+		this.price = price;
+		this.amount = amount;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+}
